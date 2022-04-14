7ae15d5c4beb1f43cbaf7cd67f641715a366c4de
Jaedon Smith <86936528+Smithjaedon@users.noreply.github.com>
2022-04-14 04:58:58 -0400

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,45 @@
 'use strict';
+let sc = 20;
 
-const message = document.querySelector('.message');
+const game = function () {
+  let guess = Number(document.querySelector('.guess').value);
+  if (guess === rndNum) {
+    message.textContent = '✅ correct';
+    document.body.style.backgroundColor = 'darkgreen';
+  } else if (guess > rndNum) {
+    message.textContent = '📈 high';
+    sc--;
+    score.textContent = sc;
+  } else if (guess < rndNum) {
+    message.textContent = '📉 low';
+    sc--;
+    score.textContent = sc;
+  }
+  if (sc === 0) {
+    guessButton.disabled = true;
+  }
+};
+
+const enter = function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    game();
+  }
+};
+
+let rndNum = Math.trunc(Math.random() * 20) + 1;
+let message = document.querySelector('.message');
+let guessButton = document.querySelector('.check');
+let againButton = document.querySelector('.again');
+const score = document.querySelector('.score');
+
+guessButton.addEventListener('click', game);
+
+guessButton.addEventListener('keyup', enter);
+
+againButton.addEventListener('click', function () {
+  rndNum = Math.trunc(Math.random() * 21) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = 0;
+  score.textContent = 20;
+});
