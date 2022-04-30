5fac684e00ead0d923ee1ba74d79595fb2aa60c4
Jaedon Smith <86936528+Smithjaedon@users.noreply.github.com>
2022-04-30 19:36:09 -0400

    new code

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,37 +1,82 @@
-import {rnd} from './functions';
-
 'use strict';
 
+//initialize
+
+
 //vars
-let rndNum = 0;
-let sc = 0;
-
+let rnd = 0;
+let sc = [0,0];
+let acitveP = 0;
+let cSc = 0;
 //players
-const p0 = document.querySelector('.player--0');
-const p1 = document.querySelector('.player--1');
-
+let p = document.querySelector(`.player--${acitveP}`);
+let p0 = document.querySelector(`.player--0`);
+let p1 = document.querySelector(`.player--1`);
+let currentP = document.querySelector('.player--active');
 //scores
 const score0 = document.querySelector('#score--0');
 const score1 = document.querySelector('#score--1');
-const curr0 = document.querySelector('#current--0');
-const curr1 = document.querySelector('#current--1');
-
+let score = document.querySelector(`#score--${acitveP}`).textContent;
+let current = document.querySelector(`#current--${acitveP}`);
+let curr0 = document.querySelector('#current--0').textContent;
+let curr1 = document.querySelector('#current--1').textContent;
 //dice
 const dice = document.querySelector('.dice');
 
-//btns
-const rollBtn = document.querySelector('.btn--roll');
+const init = function () {
+   sc = [0, 0];
+   cSc = 0;
+   acitveP = 0;
 
-//initialize
-dice.classList.add('hidden');
-document.querySelector('.score').textContent = 0;
+   score0.textContent = 0;
+   score1.textContent = 0;
+   curr0 = 0;
+   curr1 = 0;
+
+   dice.classList.add('hidden');
+   p0.classList.remove('player--winner');
+   p1.classList.remove('player--winner');
+   p0.classList.add('player--active');
+   p1.classList.remove('player--active');
+};
+
+init();
+
+//btns
+const holdBtn = document.querySelector('.btn--hold');
+const rollBtn = document.querySelector('.btn--roll');
+const newBtn = document.querySelector('.btn--new');
 
 rollBtn.addEventListener('click', function () {
-    rndNum = rnd();
-    score += rndNum;
 
-    dice.classList.remove('hidden');
-    dice.src = `dice-${rndNum}.png`;
 
-})
+   rnd = Math.trunc(Math.random()*6)+1;
 
+   dice.classList.remove('hidden');
+   dice.src = `dice-${rnd}.png`;
+
+   if (rnd !== 1) {
+      cSc += rnd;
+      document.querySelector(`#current--${acitveP}`).textContent = cSc;
+   } else {
+      cSc = 0;
+      document.querySelector(`#current--${acitveP}`).textContent = cSc;
+      acitveP = acitveP === 0 ? 1: 0;
+      p0.classList.toggle('player--active');
+      p1.classList.toggle('player--active');
+   }
+});
+
+holdBtn.addEventListener('click', function () {
+   sc[acitveP]+= cSc;
+   document.querySelector(`#score--${acitveP}`).textContent = String(sc[acitveP]);
+   cSc = 0;
+   document.querySelector(`#current--${acitveP}`).textContent = cSc;
+   if (sc[acitveP] >=100)
+      p.classList.add('player--winner');
+   acitveP = acitveP === 0 ? 1: 0;
+   p0.classList.toggle('player--active');
+   p1.classList.toggle('player--active');
+});
+
+newBtn.addEventListener('click', init);
