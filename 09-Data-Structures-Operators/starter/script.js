b3d17a74c7c497f09d9a4b673bc1e67256279990
Jaedon Smith <86936528+Smithjaedon@users.noreply.github.com>
2022-05-06 05:32:02 -0400

    data structures

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,10 +1,10 @@
-'use strict';
-
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
-// Data needed for first part of the section
+// 'use strict';
+//
+// // Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+//
+// // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -27,13 +27,94 @@ const restaurant = {
     },
   },
 };
+//
+// //deconstruct the restaurant object for opening hours
+// const { thu, fri, sat } = restaurant.openingHours;
+// const {name, location: address, mainMenu: menu, openingHours: hours} = restaurant;
+// const {
+//   fri: {open: o, close: c},
+// } = restaurant.openingHours;
+// console.log(name,address,menu,hours );
+// console.log(o,c);
+// console.log(thu,fri,sat);
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+for (const item of menu) {
+    console.log(item);
+}
 
-//deconstruct the restaurant object for opening hours
-const { thu, fri, sat } = restaurant.openingHours;
-const {name, location: address, mainMenu: menu, openingHours: hours} = restaurant;
-const {
-  fri: {open: o, close: c},
-} = restaurant.openingHours;
-console.log(name,address,menu,hours );
-console.log(o,c);
-console.log(thu,fri,sat);
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+//
+// BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+// {
+//   Gnarby: 1,
+//       Hummels: 1,
+//     Lewandowski: 2
+// }'
+
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i+1}: ${player}`);
+}
+
+let avg = 0;
+for (const odd of Object.values(game.odds)) {
+  avg += odd;
+  console.log(avg)
+}
+avg /= Object.values(game.odds).length
+console.log(avg);
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw': `victory ${game[team]}`;
+  console.log(`Odds of ${teamStr}: ${odd}`);
+}
+
+const scorers = {
+  scored: [...game.scored],
+}
